7be37452e3ddc9541ff4254a23fb80399cbe8aa6
Tomasz Banach <tomaszbanach01@gmail.com>
2018-01-06 14:47:50 +0100

    ToDo app last update

--- js/main.js
+++ js/main.js
@@ -22,10 +22,8 @@ Task.prototype.init = function (textNode) {
     container.appendChild(remover);
     remover.addEventListener("click", function (el) {
         container.parentNode.removeChild(container)
-
         var qtyOfNewContainersAfterClick = document.getElementsByClassName('frame')
-                localStorage.removeItem('frame')
-        var result = function() {
+        var result = function () {
             var empty = []
             for (var i = 0; i < qtyOfNewContainersAfterClick.length; i++)
                 empty.push(qtyOfNewContainersAfterClick[i].innerText.slice(0, -1))
@@ -33,27 +31,40 @@ Task.prototype.init = function (textNode) {
         }
         localStorage.removeItem("input")
         localStorage.setItem('input', JSON.stringify(result()))
-   })
+    })
 }
+
+function launchLocalStorage() {
+    var taskHistory = JSON.parse(localStorage.getItem("input")) || []
+    return taskHistory.forEach(function (e, i, r) {
+        var textNode = document.createTextNode(e)
+        var newInput = new Task
+        return newInput.init(textNode)
+    })
+}
+
+launchLocalStorage()
 
 function addTask() {
     var currentInput = document.getElementById('item').value
-    var textNode = document.createTextNode(currentInput)
-    var newInput = new Task
-    newInput.init(textNode)
-    var taskHistory = JSON.parse(localStorage.getItem("input")) || []
-    taskHistory.push(currentInput)
-    localStorage.setItem('input', JSON.stringify(taskHistory))
-    document.getElementById('item').value = ""
+    if (currentInput != '') {
+        var textNode = document.createTextNode(currentInput)
+        var newInput = new Task
+        newInput.init(textNode)
+        var taskHistory = JSON.parse(localStorage.getItem("input")) || []
+        taskHistory.push(currentInput)
+        localStorage.setItem('input', JSON.stringify(taskHistory))
+        document.getElementById('item').value = ""
+    }
+}
+
+function removeAll() {
+    localStorage.removeItem("input")
+    var qtyOfNewContainersAfterClick = document.getElementsByClassName('frame');
+    while (qtyOfNewContainersAfterClick[0])
+        qtyOfNewContainersAfterClick[0].parentNode.removeChild(qtyOfNewContainersAfterClick[0]);
 }
 
 document.getElementById("addbutton").addEventListener("click", addTask);
-
-function removeAll(){
-    localStorage.removeItem("input")
-    var qtyOfNewContainersAfterClick = document.getElementsByClassName('frame');
-    while(qtyOfNewContainersAfterClick[0])
-        qtyOfNewContainersAfterClick[0].parentNode.removeChild(qtyOfNewContainersAfterClick[0]);
-  }
-
 document.getElementById("removeAllTasks").addEventListener("click", removeAll);
+
